dfd986309d9e343ea06bd1de3dba6929eadf7799
Edwin Jiang <1754082565@qq.com>
2023-07-25 08:18:48 +0800

    feat: 移除所有非必要文件

--- src/store/actions.js
+++
@@ -1,7 +0,0 @@
-export default {
-  asyncChangeName(context) {
-    setTimeout(() => {
-        context.commit('asyncChangeName','刘备');
-    }, 1000);
-  }
-}
--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // computed
-  getStoreCount(state){
-    return state.count;
-  },
-  getUserInfo(state){
-    return localStorage.getItem("userInfo") || null;
-  },
-}
